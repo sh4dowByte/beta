d6d430f4d5c29cee4fe62e5bac179df97d614a4f
Ahmad Juhdi Mac <ahmadjuhdi007@gmail.com>
2024-11-13 07:28:02 +0800

    fix path script and data

--- app/config.py
+++ app/config.py
@@ -1,8 +1,17 @@
+import os
+
 class App:
     """
-    This is a simple representation of the application class, which holds the version information.
+    Represents the application configuration and paths.
 
     Attributes:
-    version (str): The current version of the application. Default is '1.0.0'.
+        version (str): Current version of the application. Default is '1.0.0'.
+        app_dir (str): Absolute path to the directory where the application is located.
+        script_path (str): Absolute path to the 'scripts' directory within the application.
+        data_path (str): Absolute path to the 'data' directory within the application.
     """
+    
     version = '1.0.0'
+    app_dir = os.path.dirname(os.path.abspath(__file__))
+    script_path = os.path.join(app_dir, 'scripts')
+    data_path = os.path.join(app_dir, 'data')
