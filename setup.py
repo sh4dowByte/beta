5fd969a9fbe3fe97c7bf3b67dbc31379bac57bdc
Ahmad Juhdi Mac <ahmadjuhdi007@gmail.com>
2025-01-07 07:07:15 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="beta",  # Nama aplikasi
+    version="1.0.0",  # Versi aplikasi
+    packages=find_packages(),  # Otomatis mencari paket di dalam folder
+    py_modules=["beta"], 
+    install_requires=[
+        "rich",
+        "requests",
+        "bs4",
+        "lxml",
+    ],
+    entry_points={
+        'console_scripts': [
+            'beta=beta:main', 
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',  # Versi Python minimum
+    author="sh4dowByte",  # Nama pembuat aplikasi
+    author_email="ahmadjuhdi007@gmail.com",  # Email pembuat (opsional)
+    description="Beta - A Python tool for scanning and information gathering",  # Deskripsi singkat
+)
